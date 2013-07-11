e9f14d7d613604c3fd198fccf679117a16732c8d
bethanyr <bethany.rentz@hotmail.com>
2013-07-10 18:35:08 -0700

    Creation of projects feature complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Ticketee::Application.routes.draw do
+
+  Ticketee::Application.routes.draw do
+    root :to => "projects#index"
+    resources :projects
+  end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
